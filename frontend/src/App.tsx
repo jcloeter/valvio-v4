89567e89b7cd89bee1e3d5de91b0d6cdef15b567
jcloeter <johncloeter@gmail.com>
2025-04-26 15:54:15 -0500

    Add image

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,12 @@ function App() {
       <div className="card">
         <h1>Valvio</h1>
 
+        <img 
+          src="./public/headshot-2.jpg" 
+          alt="john" 
+          style={{ width: '200px', height: '300px', objectFit: 'cover', borderRadius: '15px' }} 
+        />
+
         <h2>Introducing Valvio, the fastest way to learn trumpet.</h2>
 
         <p>Valvio is the world's most popular Trumpet Note Quiz App. Made by a trumpet teacher for trumpet players. </p>
