32f7c5cd3eb9655bb118624ae490e3591fdea6f9
jcloeter <johncloeter@gmail.com>
2025-04-27 22:26:17 -0500

    Create backend spring boot project

---
+++ valvio/src/main/java/com/valviomusic/valvio/ValvioApplication.java
@@ -0,0 +1,13 @@
+package com.valviomusic.valvio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValvioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValvioApplication.class, args);
+	}
+
+}
---
+++ valvio/src/test/java/com/valviomusic/valvio/ValvioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valviomusic.valvio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValvioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
